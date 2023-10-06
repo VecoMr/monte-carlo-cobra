e94ce4d7418e8113c9e965c34cb4c4c003b783c6
MrVeco <alexandre.douard@epitech.eu>
2023-10-06 13:21:44 +0200

    ADD: readme

--- monte-carlo.py
+++ monte-carlo.py
@@ -1,9 +1,7 @@
-#!/usr/bin/python3
 from src.show import *
 
-
 def main():
-    NB_ESSAI = 10000
+    NB_ESSAI = ...
 
     def simulation(nb_essai):
         nb_point_total = 0
@@ -11,16 +9,18 @@ def main():
 
         for _ in range(nb_essai):
             # On choisi au hasard notre abscisse entre 0 et 1
-            x = my_random(0, 1)
+            x = my_random(-3, 10)
             # On choisi au hasard notre ordonnée entre 0 et 1
-            y = my_random(0, 1)
+            y = my_random(-12, 1)
 
             nb_point_total += 1
 
             if x**2 + y**2 < 1:
                 nb_point_in_circle += 1
 
-        print(nb_point_in_circle / nb_point_total * 4)
+        pi = nb_point_in_circle / nb_point_total * 4
+        # Afficher l'approximation de PI
+        print(pi)
         display_round(mode="show")
     simulation(NB_ESSAI)
 
